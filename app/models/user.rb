57a16522053b99649d273f7f6cfd1b70c0730e6a
vanessuniq <vanessuniq@gmail.com>
2020-05-08 22:08:12 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+    validates :balance, numericality: {greater_than_or_equal_to: 0.00}
+    has_secure_password
+    after_initialize :defaults
+
+    def defaults
+        unless persisted?
+            self.balance ||= 0.00
+        end
+    end
+    
 end
